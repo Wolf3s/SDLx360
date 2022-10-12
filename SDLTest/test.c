721b2b08fedd8d4690bb607ed9d8f58d757727cc
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2022-10-12 18:41:39 -0300

    Initial commit

---
+++ SDLTest/test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <SDL.h>
+
+#define WIDTH 640
+#define HEIGHT 480
+#define BPP 2
+#define DEPTH 16
+
+static void setpixel(SDL_Surface *  screen, int x, int y, Uint8 r, Uint8 g, Uint8 b)
+{
+    Uint16 * __restrict pixmem32;
+    Uint16 colour;  
+ 
+    colour = SDL_MapRGB( screen->format, r, g, b );
+  
+    pixmem32 = (Uint16*) screen->pixels  + y + x;
+    *pixmem32 = colour;
+}
+
+
+void DrawScreen(SDL_Surface* __restrict  screen, int h)
+{ 
+    int x, y, ytimesw;
+  
+    if(SDL_MUSTLOCK(screen)) 
+    {
+        if(SDL_LockSurface(screen) < 0) return;
+    }
+
+    for(y = 0; y < screen->h; y++ ) 
+    {
+        ytimesw = y*screen->pitch>>1;
+        for( x = 0; x < screen->w; x++ ) 
+        {
+            setpixel(screen, x, ytimesw, (x<<2)>>8+y+h, (y<<2)>>8+x+h, h);
+        }
+    }
+
+    if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+  
+    SDL_Flip(screen); 
+}
+
+
+int main()
+{
+    SDL_Surface * __restrict screen;
+    SDL_Event event;
+  
+    int keypress = 0;
+    int h=0; 
+  
+    if (SDL_Init(SDL_INIT_VIDEO) < 0 ) return 1;
+   
+    if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_FULLSCREEN|SDL_HWSURFACE|SDL_DOUBLEBUF)))
+    {
+        SDL_Quit();
+        return 1;
+    }
+  
+    while(!keypress) 
+    {
+         DrawScreen(screen,h++);
+        
+    }
+
+    SDL_Quit();
+  
+    return 0;
+}
